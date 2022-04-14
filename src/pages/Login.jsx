f8d3795f65aa4625b7b38976eb5584825c5fefed
Prashant Singh <v-singhpras@microsoft.com>
2022-04-14 11:29:42 +0530

    Adding the router

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <form>
+            <input type="username"></input>
+            <input type="password"></input>
+            <input type="submit"></input>
+        </form>
+    </div>
+  )
+}
